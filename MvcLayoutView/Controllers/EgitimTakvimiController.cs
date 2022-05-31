036dfb435f784c916c1fb701cbb1ed7c1f9c3278
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-05-31 21:34:41 +0300

    Add project files.

---
+++ MvcLayoutView/Controllers/EgitimTakvimiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcLayoutView.Controllers
+{
+    public class EgitimTakvimiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
